844e3fb878529919eebbd4fa4025e5259507b67b
ferdoctor <ferdoctor@gmail.com>
2015-01-19 13:15:25 +0000

    Fixed licence

--- numpy_popcnt.c
+++ numpy_popcnt.c
@@ -1,4 +1,23 @@
-/* Module to do population count of bits in numpy arrays */
+/* NUMPY_POPCNT
+*
+* Copyright (c) 2014-2015 Dr. Fernando Rodriguez Salazar
+*
+* This file is part of NUMPY_POPCNT, a numpy extension to do population 
+* count of bits in numpy arrays.
+*
+*    NUMPY_POPCNT is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 2 of the License, or
+*    (at your option) any later version.
+*
+*    NUMPY_POPCNT is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with NUMPY_POPCNT.  If not, see <http://www.gnu.org/licenses/>
+*/
 
 #include "Python.h"
 #include <numpy/arrayobject.h>
--- numpy_popcnt.h
+++ numpy_popcnt.h
@@ -1,4 +1,23 @@
-/* Header to test of C modules for arrays for Python: C_test.c */
+/* NUMPY_POPCNT
+*
+* Copyright (c) 2014-2015 Dr. Fernando Rodriguez Salazar
+*
+* This file is part of NUMPY_POPCNT, a numpy extension to do population 
+* count of bits in numpy arrays.
+*
+*    NUMPY_POPCNT is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 2 of the License, or
+*    (at your option) any later version.
+*
+*    NUMPY_POPCNT is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with NUMPY_POPCNT.  If not, see <http://www.gnu.org/licenses/>
+*/
 
 
 const int8_t numbitsData[256]={0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4, 1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5, 1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7, 1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7, 3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7, 4, 5, 5, 6, 5, 6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8};
